d9d49f2da2cc056703d3b58c828d1d68a3caabc2
RodryAlexander <rodrygoleu7@gmail.com>
2025-08-01 11:32:42 -0500

    Agregar funcion para añadir amigos validando las entradas

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+const agregarAmigo = () =>{
+   const input = document.getElementById('amigo');
+   const amigo = input.value.trim() // removemos espacios
+   if(amigo === '' ) alert('Ingrese un nombre valido');
+   else amigos.push(amigo);
+   input.innerHTML = '';
+   console.log(amigos)
+}
+
