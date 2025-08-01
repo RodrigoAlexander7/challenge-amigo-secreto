4fdf8da1dfaa06d80925b2527c97f749be3c420c
RodryAlexander <rodrygoleu7@gmail.com>
2025-08-01 11:45:37 -0500

    Agregar validacion para duplicados

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ let amigos = []
 const agregarAmigo = () =>{
    const input = document.getElementById('amigo');
    const amigo = input.value.trim() // removemos espacios
-   if(amigo === '') alert('Ingrese un nombre valido');
-   else amigos.push(amigo);
+   if(amigo !== '' && !amigos.includes(amigo)) amigos.push(amigo);
+   else alert('Ingrese un nombre valido'); 
    input.value = '';
    console.log(amigos)
    actualizarListaAmigos(amigo)
